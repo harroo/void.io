624b514008f201f175e0e3f061a48c37311648a9
harroo <harroo@protonmail.com>
2021-10-05 17:26:18 +1100

    fix sprite animation not playing last frame

--- void-project/Assets/Scripts/Utility/Universal/SpriteAnimation.cs
+++ void-project/Assets/Scripts/Utility/Universal/SpriteAnimation.cs
@@ -21,13 +21,16 @@ public class SpriteAnimation : MonoBehaviour {
 
         if (timer < 0) { timer = frameDelay;
 
-            image.sprite = frames[index++];
-
             if (index == frames.Length) {
 
                 if (loop) index = 0;
                 else if (destroyOnFinish) Destroy(gameObject);
+
+                timer = 0;
+                return;
             }
+
+            image.sprite = frames[index++];
 
         } else timer -= Time.deltaTime;
     }
