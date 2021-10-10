d1054f1e26616d411c4af970731929995371e8d3
harroo <harroo@protonmail.com>
2021-10-10 15:05:34 +1100

    improve upgrade menu

--- void-project/Assets/Scripts/Gameplay/Menu/Upgrades/UpgradeDetector.cs
+++ void-project/Assets/Scripts/Gameplay/Menu/Upgrades/UpgradeDetector.cs
@@ -1,17 +1,34 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeDetector : MonoBehaviour {
 
     public GameObject upgradeOverlay;
 
+    public Text pointsReminder;
+
     private float timer;
     private void Update () {
 
-        if (timer < 0) { timer = 0.5f;
+        if (timer < 0) { timer = 0.25f;
 
-            upgradeOverlay.SetActive(PlanetIndex.CheckIfInRange(transform.position, 0.24f) && !Input.anyKey);
+            upgradeOverlay.SetActive(PlanetIndex.CheckIfInRange(transform.position, 0.24f) && MoveCheck());
 
         } else timer -= Time.deltaTime;
+
+
+        pointsReminder.text =
+            UpgradeMenu.instance.points > 0 ? "You've " +
+            UpgradeMenu.instance.points.ToString() +
+            " unspent point" +
+            (UpgradeMenu.instance.points == 1 ? "" : "s") +
+            ". Idle near a planet to spend " +
+            (UpgradeMenu.instance.points == 1 ? "it" : "them") +
+            "..." : "";
     }
+
+    private bool MoveCheck ()
+        => !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) &&
+            !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D);
 }
