98663e4568f327cae112ab5ecdcc4d13fc7a2e70
harroo <harroo@protonmail.com>
2021-10-24 13:46:39 +1100

    put some info for when i come back

--- void-project/Assets/Scripts/Gameplay/Menu/ShipDocks/ShipdocksMenu.cs
+++ void-project/Assets/Scripts/Gameplay/Menu/ShipDocks/ShipdocksMenu.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 
 public class ShipdocksMenu : MonoBehaviour {
 
-    
+    //check if player can do upgrade thing, so like if the player has 10 or more points or something
+    //
+    //player gets all stats reset when they upgrade, maybe, change there stat thing
 }
