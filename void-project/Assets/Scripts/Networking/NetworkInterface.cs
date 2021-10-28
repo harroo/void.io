8ec83399a993a5e531bc4c004aba9b6cdf3eafb9
harroo <harroo@protonmail.com>
2021-10-28 14:13:44 +1100

    fix bug that causes colliders to not render properly when first joining

--- void-project/Assets/Scripts/Networking/NetworkInterface.cs
+++ void-project/Assets/Scripts/Networking/NetworkInterface.cs
@@ -22,6 +22,9 @@ public class NetworkInterface : MonoBehaviour {
         GlobalValues.LocalPlayerID = UnityEngine.Random.Range(111111111, 999999999);
         PlayerManager.instance.SetPlayerStart(GlobalValues.LocalPlayerID);
         TcpStream.Send_CreateObject(GlobalValues.LocalPlayerID, 0, PlayerManager.instance.GetPlayerData());
+
+        //might be removed and used just as the default thing when we have multiple tech trees
+        TcpStream.Send_ColliderUpdate(GlobalValues.LocalPlayerID, 1);
     }
 
     public void Update () {
